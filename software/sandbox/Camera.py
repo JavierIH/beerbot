59cac6d15813dd699a0949a5246fa4f9f1c42af3
David-Estevez <david.estevez.fdez@gmail.com>
2015-05-25 18:53:19 +0200

    Fixed hsv intevals

--- software/sandbox/Camera.py
+++ software/sandbox/Camera.py
@@ -1,22 +1,32 @@
 import numpy as np
 import cv2
-from CalibratedVideoCapture import CalibratedVideoCapture
+from CameraCalibration.CalibratedVideoCapture import CalibratedVideoCapture
+
 
 __author__ = 'kfrodicio'
 
 
 class Camera:
     def __init__(self, id, camera_parameters_file):
-	self.camera = CalibratedVideoCapture()
-	self.camera.open(id, camera_parameters_file)
-        return
+        self.camera = CalibratedVideoCapture()
+        self.camera.open(id, camera_parameters_file)
+
+        self.black_ub = (180, 40, 127)
+        self.black_lb = (0, 0, 0)
+        self.blue_ub = (122, 255, 200)
+        self.blue_lb = (62, 75, 66)
+        self.brown_ub = (55, 89, 89)
+        self.brown_lb = (5, 5, 65)
+        self.green_ub = (90, 255, 255)
+        self.green_lb = (43, 40, 66)
 
     def get_binary_map(self):
-        image = self.camera.read()
-        green = cv2.inRange(cv2.cvtColor(image, cv2.COLOR_BGR2HSV), np.array([43, 40, 66], np.uint8), np.array([74, 255, 255], np.uint8))
-        brown = cv2.inRange(cv2.cvtColor(image, cv2.COLOR_BGR2HSV), np.array([5, 5, 65], np.uint8), np.array([55, 89, 89], np.uint8))
-        blue = cv2.inRange(cv2.cvtColor(image, cv2.COLOR_BGR2HSV), np.array([111, 75, 127], np.uint8), np.array([125, 160, 243], np.uint8))
-        black = cv2.inRange(cv2.cvtColor(image, cv2.COLOR_BGR2HSV), np.array([110, 0, 74], np.uint8), np.array([177, 60, 116], np.uint8))
+        dummy, image = self.camera.read()
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        green = cv2.inRange(hsv, self.green_lb, self.green_ub)
+        brown = cv2.inRange(hsv, self.brown_lb, self.brown_ub)
+        blue = cv2.inRange(hsv, self.blue_lb, self.blue_ub)
+        black = cv2.inRange(hsv, self.black_lb, self.black_ub)
         bin_env = cv2.bitwise_or(green, brown)
         bin_env = cv2.bitwise_or(bin_env, blue)
         bin_env = cv2.bitwise_or(bin_env, black)
@@ -26,9 +36,9 @@ class Camera:
         return image_bin
 
     def get_robot_pose(self):
-        image = self.camera.read()
-        blue = cv2.inRange(cv2.cvtColor(image, cv2.COLOR_BGR2HSV), np.array([111, 75, 127], np.uint8), np.array([125, 160, 243], np.uint8))
-        black = cv2.inRange(cv2.cvtColor(image, cv2.COLOR_BGR2HSV), np.array([110, 0, 74], np.uint8), np.array([177, 60, 116], np.uint8))
+        dummy, image = self.camera.read()
+        blue = cv2.inRange(cv2.cvtColor(image, cv2.COLOR_BGR2HSV), self.blue_lb, self.blue_ub)
+        black = cv2.inRange(cv2.cvtColor(image, cv2.COLOR_BGR2HSV), self.black_lb, self.black_ub)
         robot = cv2.bitwise_or(blue, black)
         robot = cv2.erode(robot, np.ones((3, 3), np.uint8), iterations=2)
         robot = cv2.dilate(robot, np.ones((3, 3), np.uint8), iterations=4)
@@ -36,11 +46,10 @@ class Camera:
         cnt = contours[0]
         (x, y), radius = cv2.minEnclosingCircle(cnt)
         orientation = self.get_orientation(blue, black)
-        orientation = 360 - orientation
         return int(x), int(y), orientation
 
     def get_goal_pos(self):
-        image = self.camera.read()
+        dummy, image = self.camera.read()
         red = cv2.inRange(cv2.cvtColor(image, cv2.COLOR_BGR2HSV), np.array([0, 49, 83], np.uint8), np.array([21, 255, 190], np.uint8))
         goal = cv2.erode(red, np.ones((3, 3), np.uint8), iterations=2)
         goal = cv2.dilate(goal, np.ones((3, 3), np.uint8), iterations=2)
@@ -56,6 +65,8 @@ class Camera:
         black = cv2.dilate(black, np.ones((3, 3), np.uint8), iterations=5)
         contours_a, hierarchy_a = cv2.findContours(blue, 1, 2)
         contours_n, hierarchy_n = cv2.findContours(black, 1, 2)
+        if not contours_a or not contours_n:
+            return None
         cnt_a = contours_a[0]
         cnt_n = contours_n[0]
         rect = cv2.minAreaRect(cnt_a)
@@ -64,19 +75,19 @@ class Camera:
         ang = int(rect[2]) + 90
         if 0 <= ang <= 45:
             if x_a > x_n:
-                return ang
+                return 360-ang
             else:
                 ang += 180
-                return ang
+                return 360-ang
         elif 45 < ang <= 135:
             if y_a > y_n:
-                return ang
+                return 360-ang
             else:
                 ang += 180
-                return ang
+                return 360-ang
         elif 135 < ang <= 180:
             if x_a < x_n:
-                return ang
+                return 360-ang
             else:
                 ang += 180
-                return ang
+                return 360-ang
--- software/sandbox/Prueba.py
+++ software/sandbox/Prueba.py
@@ -5,11 +5,15 @@ __author__ = 'kike'
 
 
 def main():
-    camera = Camera()
+    camera = Camera(1, './CameraCalibration/logitech/calibration_image.npz')
     while 1:
         image = camera.get_binary_map()
         cv2.imshow('mapa', image)
         pose = camera.get_robot_pose()
+        k = cv2.waitKey(1) & 0xFF
+        if k == ord('q'):
+            break
+
         print('posicion: ', pose[0], pose[1])
         print('orientacion: ', pose[2])
 
